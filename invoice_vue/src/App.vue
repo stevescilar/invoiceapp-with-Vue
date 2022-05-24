d8f5bbad46c6c496ccfbeb5a115bf9062f01fb48
stevescilar <stevemuambi@gmail.com>
2022-05-24 16:14:50 +0300

    Updated

--- invoice_vue/src/App.vue
+++ invoice_vue/src/App.vue
@@ -1,30 +1,14 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>MyInvoice</strong></router-link>
+      </div>
+    </nav>
+  </div>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import '../node_modules/bulma';
 </style>
--- invoice_vue/src/store/index.js
+++ invoice_vue/src/store/index.js
@@ -15,7 +15,18 @@ export default createStore({
       if(localStorage.getItem('token')){
         state.token = localStorage.getItem('token')
         state.isAuthenticated = true
+      }else {
+        state.token = ''
+        state.isAuthenticated = false
       }
+    },
+    setToken(state, token) {
+      state.token = token
+      state.isAuthenticated = true
+    },
+    removeToken(state){
+      state.token = ''
+      state.isAuthenticated = false
     }
   },
   actions: {
