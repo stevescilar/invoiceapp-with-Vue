9bec1e159554001157e8d48037150253cfeb2164
stevescilar <stevemuambi@gmail.com>
2022-05-24 15:18:42 +0300

    Updated

--- invoice_vue/src/store/index.js
+++ invoice_vue/src/store/index.js
@@ -2,10 +2,21 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user: {
+      username: ''
+    },
+    isAuthenticated: false,
+    token: ''
   },
   getters: {
   },
   mutations: {
+    initializeStore(state) {
+      if(localStorage.getItem('token')){
+        state.token = localStorage.getItem('token')
+        state.isAuthenticated = true
+      }
+    }
   },
   actions: {
   },
