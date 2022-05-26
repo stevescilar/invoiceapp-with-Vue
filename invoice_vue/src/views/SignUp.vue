b075ff38dd143fc2be96c6b2474f88cf7466bcf5
stevescilar <stevemuambi@gmail.com>
2022-05-26 09:18:19 +0300

    Updated

--- invoice_vue/src/views/SignUp.vue
+++ invoice_vue/src/views/SignUp.vue
@@ -1,5 +1,26 @@
 <template>
-    
+    <div class="page-signup">
+        <div class="columns">
+            <div class="column is-4 is-offset-4">
+                <h1 class="title">Sign Up</h1>
+
+                <form @submit.prevent="submitForm">
+                    <div class="field">
+                        <label for="">E-mail</label>
+                        <div class="control">
+                            <input type="email" name="username" id="" class="input" v-model="username">
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label for="">Password</label>
+                        <div class="control">
+                            <input type="password" name="password" id="" class="input" v-model="password">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
