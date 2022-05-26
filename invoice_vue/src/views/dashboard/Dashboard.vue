ea6ef434b56ab8d6628636cf8a0be3cc3ccb16a0
stevescilar <stevemuambi@gmail.com>
2022-05-26 09:10:35 +0300

    Updated

--- invoice_vue/src/views/dashboard/Dashboard.vue
+++ invoice_vue/src/views/dashboard/Dashboard.vue
@@ -1,1 +1,14 @@
-<h4>grace</h4>
+<template>
+    <div class="page-dashboard">
+        <div class="columns is-multiline">
+            <div class="column is-12">
+                <h1 class="title">Dashboard</h1>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default{
+    name:'Dashboard' 
+}
+</script>
