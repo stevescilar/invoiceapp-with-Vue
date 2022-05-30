fd77e0e80e73bd5c6e0a5330404a4ab947ea7c82
stevescilar <stevemuambi@gmail.com>
2022-05-30 11:07:37 +0300

    Updated

--- invoice_vue/src/views/dashboard/Dashboard.vue
+++ invoice_vue/src/views/dashboard/Dashboard.vue
@@ -2,7 +2,7 @@
     <div class="page-dashboard">
         <div class="columns is-multiline">
             <div class="column is-12">
-                <h1 class="title">Dashboard</h1>
+                <h1 class="title">Dashboard-Home</h1>
             </div>
         </div>
     </div>
